946bdef0528c7ebb345a765863fd0d4db6708f21
Patricia Serti <patricia_serti@hotmail.com>
2022-06-12 21:11:21 +0200

    user class added

---
+++ MvcApi/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_End.Model
+
+public class User
+{
+	{
+	public int Id { get; set; }
+	public string Userame { get; set; }
+	public string Password { get; set; }
+	public List<User> Users { get; set; }
+	}
+}
